9668361fc5cffdf5584ea9d3884d39403f32f458
saarboledaz <sergioarboledazuluaga@gmail.com>
2016-05-11 22:13:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt4 import QtGui
+import sys
+import form3qt
+
+class Form3(QtGui.QMainWindow, form3qt.Ui_MainWindow):
+	def __init__(self,parent =None):
+		super(Form3, self).__init__(parent)
+		self.setupUi(self)
+
+def main():
+	app = QtGui.QApplication(sys.argv)
+	form = Form3()
+	form.show()
+	app.exec_()
+
+if __name__ == '__main__':
+    main()
